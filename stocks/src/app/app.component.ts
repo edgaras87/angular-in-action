472898ed7bca41e970eac428c0d694cd05b010f1
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-23 18:25:29 +0200

    Create a component that displays stock price information
    Create a component that displays a basic summary card of the stock price information using stock service.

--- stocks/src/app/app.component.ts
+++ stocks/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StocksService, StockInterface } from './services/stocks.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my stocks';
+
+  stocks: Array<StockInterface> = [];
+
+  constructor(service: StocksService) {
+    service.load(['AAPL'])?.subscribe(stocks => {
+      this.stocks = stocks;
+    });
+
+  }
+
 }
