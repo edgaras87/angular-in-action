9b54d7de3b2f607bce49e92073763293e5631e63
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-23 19:42:26 +0200

    Create a manage component
    Component that has a form to edit the list of stocks.

---
+++ stocks/src/app/components/manage/manage.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { StocksService } from 'src/app/services/stocks.service';
+
+@Component({
+  selector: 'manage',
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.css']
+})
+export class ManageComponent {
+
+  symbols: Array<string> = [];
+  stock: string = "";
+
+  constructor(private service: StocksService) {
+    this.symbols = service.get();
+  }
+
+  add() {
+    this.symbols = this.service.add(this.stock.toUpperCase());
+  }
+
+  remove(symbol: string) {
+    this.symbols = this.service.remove(symbol);
+  }
+  
+
+}
