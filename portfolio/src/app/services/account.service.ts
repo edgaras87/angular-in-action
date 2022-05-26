15eee42f2fa45b48ea074df3f3e90e5ccf2d4c8f
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-26 19:44:22 +0200

    Add account reset functionality

--- portfolio/src/app/services/account.service.ts
+++ portfolio/src/app/services/account.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Stock } from './stocks.model';
 
 const defaultBalance: number = 10000;
 
 @Injectable()
 export class AccountService {
 
-  private _balanace: number = defaultBalance;
+  private _balance: number = defaultBalance;
   private _cost: number = 0;
   private _value: number = 0;
 
+  get balance(): number { return this._balance; }
+  get cost(): number { return this._cost; }
+  get value(): number { return this._value; }
+
   init() {
 
+  }
+
+  reset() {
+    this._balance = defaultBalance;
+    this._value = this._cost = 0;
   }
 
 }
