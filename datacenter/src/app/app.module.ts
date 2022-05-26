9db4464df51a8e2a963f90aa73e981b2048773e6
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-26 09:54:36 +0200

    Dynamic component. Single cluster information in seperate component
    Render dynamic component using prebuilt the Ng-bootstrap modal service.

--- datacenter/src/app/app.module.ts
+++ datacenter/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MetricComponent } from './metric/metric.component';
 import { NodesComponent } from './nodes/nodes.component';
 import { NodesRowComponent } from './nodes-row/nodes-row.component';
+import { NodesDetailComponent } from './nodes-detail/nodes-detail.component';
 
 
 @NgModule({
@@ -17,11 +18,15 @@ import { NodesRowComponent } from './nodes-row/nodes-row.component';
     DashboardComponent,
     MetricComponent,
     NodesComponent,
-    NodesRowComponent
+    NodesRowComponent,
+    NodesDetailComponent
   ],
   imports: [
     BrowserModule,
     NgbModule
+  ],
+  entryComponents: [
+    NodesDetailComponent
   ],
   providers: [],
   bootstrap: [AppComponent],
--- datacenter/src/app/nodes-row/nodes-row.component.ts
+++ datacenter/src/app/nodes-row/nodes-row.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NodesDetailComponent } from '../nodes-detail/nodes-detail.component';
 
 @Component({
   selector: '[app-nodes-row]',
@@ -9,9 +11,16 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 export class NodesRowComponent {
 
   @Input() node: any;
+
+  constructor(private modalService: NgbModal) {}
    
   isDanger(prop: string) {
     return this.node[prop].used / this.node[prop].available > 0.7;
+  }
+
+  open(node: any) {
+    const modal = this.modalService.open(NodesDetailComponent);
+    modal.componentInstance.node = node;
   }
 
 }
