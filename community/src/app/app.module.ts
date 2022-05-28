6e615a5ae450a61f7b04d45678bddc7eb407f31b
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-28 11:44:19 +0200

    Create login page
    Login page is routed with RouterModule to router outlet.

--- community/src/app/app.module.ts
+++ community/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
+const appRouters: Routes = [
+  { path: 'login', component: LoginComponent }
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    ClarityModule
+    ClarityModule,
+    RouterModule.forRoot(appRouters)
   ],
   providers: [],
   bootstrap: [AppComponent]
