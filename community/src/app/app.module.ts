28322f00f2939c108257755d5fca44f9be7aca62
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-28 13:51:47 +0200

    Add user functionality to project
    Create user service to implement user functionality to project.

--- community/src/app/app.module.ts
+++ community/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { UserService } from './services/user.service';
 
 const appRouters: Routes = [
   { path: 'login', component: LoginComponent }
 ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     ClarityModule,
-    RouterModule.forRoot(appRouters)
+    RouterModule.forRoot(appRouters),
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- community/src/app/login/login.component.ts
+++ community/src/app/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent  {
+export class LoginComponent {
 
+  username: string = '';
+  password: string = '';
+  return: string = '';
+
+  constructor(private userService: UserService,
+              private router: Router) {}
+
+
+  login() {
+    if(this.username && this.password) {
+      // here "this" points to ngModel that is model created by login form 
+      console.log("username: " + this.username);
+      console.log("password: " + this.password);
+      this.userService.login(this.username);
+      this.go();
+    }
+  }
+
+  private go() {
+    this.router.navigateByUrl(this.return);
+  }
 
 }
