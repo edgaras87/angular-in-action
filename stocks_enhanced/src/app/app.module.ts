fbd8586faa8dfce21e357f3afd47b2bc764be7f3
edgaras <stasiukaitisedgaras87@gmail.com>
2022-06-02 09:35:08 +0200

    Attribute directive. Color of the summary component
    Create an attribute directive to change the color of the summary component.

--- stocks_enhanced/src/app/app.module.ts
+++ stocks_enhanced/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { StocksService } from './services/stocks.service';
@@ -9,18 +10,21 @@ import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ManageComponent } from './components/manage/manage.component';
 import { AppRoutes } from './app.routes';
+import { CardTypeDirective } from './directives/card-type.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
     DashboardComponent,
-    ManageComponent
+    ManageComponent,
+    CardTypeDirective,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
     AppRoutes
   ],
   providers: [StocksService],
--- stocks_enhanced/src/app/components/summary/summary.component.ts
+++ stocks_enhanced/src/app/components/summary/summary.component.ts
@@ -1,30 +1,36 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.css']
+  styleUrls: ['./summary.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      state('in', style({opacity: 1})),
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(300)
+      ])
+    ])
+  ]
 })
 export class SummaryComponent {
 
   @Input() stock: any;
 
-  isNegative() {
+  // isNegative() {
+  //   if(!this.stock || this.stock.change >= 0) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
 
-    if(!this.stock || this.stock.change >= 0) {
-      return false;
-    }
-
-    return true;
-  }
-
-  isPositive() {
-
-    if(!this.stock || this.stock.change <= 0) {
-      return false;
-    }
-
-    return true;
-  }
+  // isPositive() {
+  //   if(!this.stock || this.stock.change <= 0) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
 
 }
