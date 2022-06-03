75c2838e6d753bbbc500c040d5b1cd94df213da9
edgaras <stasiukaitisedgaras87@gmail.com>
2022-06-03 12:47:42 +0200

    Add navigation bar

--- invoice/src/app/app.module.ts
+++ invoice/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
-
 import { AppComponent } from './app.component';
+import { CovalentLayoutModule, CovalentStepsModule } from '@covalent/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CovalentLayoutModule,
+    CovalentStepsModule
   ],
   providers: [],
   bootstrap: [
